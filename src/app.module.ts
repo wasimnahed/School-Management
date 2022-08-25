7a160526d5c130311b1250ce8b976a95a013d687
wasimnahed <Wasim.Nahed@softobiz.com>
2022-08-25 17:01:15 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { LessonModule } from './lesson/lesson.module';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson, Student],
+    }),
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
+})
+export class AppModule {}
---
+++ src/student/student.type.ts
@@ -0,0 +1,11 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+
+@ObjectType('Student')
+export class StudentType {
+    @Field()
+    id: string;
+    @Field()
+    firstName: string;
+    @Field()
+    lastName: string;
+}
